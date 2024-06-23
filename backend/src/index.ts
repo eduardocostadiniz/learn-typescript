0357b559b59ea233eb90704f3aea1a211de6a1ec
Eduardo Diniz <eduardocostadiniz67@gmail.com>
2024-06-23 17:09:10 -0300

    feat: Adicionar ESlint no projeto

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,10 @@
-import express from "express";
+import express from 'express';
 
-const app = express()
+const app = express();
 
 app.get('/', (req, res) => {
-    console.log(req.body)
-    return res.send({msg: 'Olá Mundo'})
-})
+  console.log(req.body);
+  return res.send({ msg: 'Olá Mundo' });
+});
 
-app.listen(3333, () => console.log('Aplicação Iniciada!'))
+app.listen(3333, () => console.log('Aplicação Iniciada!'));
