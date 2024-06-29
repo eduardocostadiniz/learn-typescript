ff401b590b0cc6e4edde2c142e2cdae4467bde8e
Eduardo Diniz <eduardocostadiniz67@gmail.com>
2024-06-29 08:26:01 -0300

    feat: Organizando as rotas do projeto

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,9 @@
 import express from 'express';
+import { router } from './routes';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  console.log(req.body);
-  return res.send({ msg: 'Olá Mundo' });
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => console.log('Aplicação Iniciada!'));
---
+++ backend/src/routes/index.ts
@@ -0,0 +1,19 @@
+import { Router } from 'express';
+import { StatusCodes} from 'http-status-codes';
+
+
+const router = Router();
+
+router.get('/', (req, res) => {
+  console.log(req.body);
+  return res.send({ msg: 'Olá Mundo' });
+});
+
+router.post('/teste', (req, res) => {
+  console.log(`QUERY: ${req.query}`);
+  console.log(`BODY: ${req.body}`);
+  return res.status(StatusCodes.CREATED).send({ query: req.query, body: req.body });
+});
+
+
+export { router };
