5470767b446cb0f79df0a6c820ac3abe8e506f78
Eduardo Diniz <eduardocostadiniz67@gmail.com>
2024-07-15 17:04:05 -0300

    feat: Adicionando seed para cidades no projeto

---
+++ backend/src/database/seeds/0000_create_cidades.ts
@@ -0,0 +1,29 @@
+import { ETableNames } from '../ETableName';
+import { Knex } from '../knex';
+
+
+export const seed = async () => {
+  const count = await Knex(ETableNames.CIDADE).count('* as total').select().first<{ total: number }>();
+
+  if (!Number.isInteger(count.total) || count.total > 0) return;
+
+  const cidades = cidadesMG.map(nomeCidade => ({ nome: nomeCidade }));
+
+  await Knex(ETableNames.CIDADE).insert(cidades);
+
+};
+
+const cidadesMG = [
+  'Araguari',
+  'Ponte Nova',
+  'Três Corações',
+  'Três Marias',
+  'Três Pontas',
+  'Tumiritinga',
+  'Tupaciguara',
+  'Ubaí',
+  'Ubaporanga',
+  'Uberaba',
+  'Uberlândia',
+  'Umburatiba',
+];
